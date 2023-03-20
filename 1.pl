55eb5c8b6a1a2cd80a3f56e8b2f2d856efad78d0
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2023-03-19 23:26:14 -0300

    Update 1.pl

--- 1.pl
+++ 1.pl
@@ -1,1 +1,15 @@
 par(X):- X mod 2 =:= 0. %Verifica se o valor X é par
+
+%--------------------------------------------------------
+
+idade(john, 30).
+idade(sarah, 25).
+
+comparar_idades(Person1, Person2, Comparison) :- %Comparar idades
+    idade(Person1, Age1),
+    idade(Person2, Age2),
+    (
+        Age1 > Age2 -> Comparison = older;
+        Age1 < Age2 -> Comparison = younger;
+        Comparison = same
+    ).

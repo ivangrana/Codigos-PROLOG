e99efa4742499900227b62685500c364047b6f66
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2023-03-27 10:49:35 -0300

    Update 1.pl

--- recursão/1.pl
+++ recursão/1.pl
@@ -4,3 +4,12 @@ fatorial(1, 1) :- !.
 fatorial(N, R) :- N1 is N-1,
 fatorial(N1, R1),
 R is R1 * N.
+
+soma_ate_zero(Soma) :-
+    write('Digite um valor: '), %Print
+    read(Numero), %recebe o valor na variável Número
+    (
+        Numero =:= 0 -> Soma = 0;
+        soma_ate_zero(SomaAnterior), %Chama novamente o soma_ate_zero
+        Soma is Numero + SomaAnterior
+    ).

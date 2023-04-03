4d231a3fe219fa69860699157e05e6ae8151fd95
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2023-04-03 12:59:21 -0300

    Update Lista_exercicios.pl

--- listas/Lista_exercicios.pl
+++ listas/Lista_exercicios.pl
@@ -13,3 +13,40 @@ contar([H|T], X, N) :-
 concat([], L, L).
 concat([H|T], L, [H|Result]) :-
     concat(T, L, Result).
+    
+ 
+    
+% Questão 5
+
+ ultimo(X,[X]).
+ Ultimo(X,[_|Z]) :- Ultimo(X,Z)
+
+
+% Questão 7 
+
+reverse([],Z,Z).
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+
+% Questão 8
+
+member(X,[X,_]).
+member(X,[_,T]) :- member(X,T).
+
+% Questão 9
+
+repeated(L) :-
+    sort(L, S),
+    length(L, X),
+    length(S, Y),
+    X > Y.
+
+% QUestão 10
+
+remove_duplicates([], []).
+
+remove_duplicates([Head | Tail], Result) :-
+    member(Head, Tail), !,
+    remove_duplicates(Tail, Result).
+
+remove_duplicates([Head | Tail], [Head | Result]) :-
+    remove_duplicates(Tail, Result).

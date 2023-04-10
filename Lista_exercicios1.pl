d37de700f33fa9495be6f18f319ad90cebdbd62f
Ivan Grana <89265428+ivangrana@users.noreply.github.com>
2023-04-09 21:41:23 -0300

    Update and rename 1.pl to Lista_exercicios1.pl

---
+++ Lista_exercicios1.pl
@@ -0,0 +1,57 @@
+%Questão 3
+
+merge_sort([], []).
+merge_sort([X], [X]).
+merge_sort(List, Sorted) :-
+    length(List, Len),
+    Len > 1,
+    split(List, Left, Right),
+    merge_sort(Left, SortedLeft),
+    merge_sort(Right, SortedRight),
+    merge(SortedLeft, SortedRight, Sorted).
+
+split(List, Left, Right) :-
+    length(List, Len),
+    Half is Len // 2,
+    length(Left, Half),
+    append(Left, Right, List).
+
+merge([], Right, Right).
+merge(Left, [], Left).
+merge([X|Left], [Y|Right], [X|Sorted]) :-
+    X =< Y,
+    merge(Left, [Y|Right], Sorted).
+merge([X|Left], [Y|Right], [Y|Sorted]) :-
+    X > Y,
+    merge([X|Left], Right, Sorted).
+
+
+%Questão 4
+
+is_sorted([]). %Recebe a lista
+is_sorted([_]).
+is_sorted([X,Y|Cauda]) :- %Divide a lista em cabeça([X,Y]) e cauda
+    (   X =< Y, %se o valor do primeiro elemento for menor ou igual ao segundo.
+    is_sorted([Y|Cauda])). %Chama recursivamente
+
+%Questão 7
+
+concat(Nome, Telefone, Resultado) :-
+    string_concat("Nome: ", Nome, Parte1), %concatena com o nome fornecido para criar a Parte1
+    string_concat(Parte1, " - Telefone: ", Parte2), %concatena com o telefone fornecido para criar a Parte2
+    string_concat(Parte2, Telefone, Resultado).
+
+
+%Questão 13
+slice(Lista, Indice_inicial, Indice_final, Sliced) :-
+    length(Lista, Length),
+    Indice_final =< Length,
+    Indice_inicial =< Indice_final,
+    length(Sliced, SliceLength),
+    SlicingStart is Indice_inicial - 1,
+    _SlicingEnd is Indice_final - 1,
+    length(Prefix, SlicingStart),
+    append(Prefix, SlicedPrefix, Lista),
+    length(SlicedPrefix, SliceLength),
+    append(Sliced, _, SlicedPrefix),
+    length(Sliced, SliceLength).
